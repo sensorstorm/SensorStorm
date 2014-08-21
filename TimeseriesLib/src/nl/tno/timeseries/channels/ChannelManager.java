3b496ca0e90632675e1d6f9f859117653bcdd92c
Bram van der Waaij <bram.vanderwaaij@tno.nl>
2014-08-21 09:13:04 +0200

    MetaParticleHandlers now get only MetaParticles they have specified in the annotations

--- TimeseriesLib/src/nl/tno/timeseries/channels/ChannelManager.java
+++ TimeseriesLib/src/nl/tno/timeseries/channels/ChannelManager.java
@@ -21,6 +21,13 @@ import nl.tno.timeseries.particles.MetaParticleHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A channelManager manages for a specific channel the operation instance and
+ * optional batcher and optional one or more metahandlers.
+ * 
+ * @author waaijbdvd
+ * 
+ */
 public class ChannelManager implements Serializable {
 	protected Logger logger = LoggerFactory.getLogger(ChannelManager.class);
 
@@ -37,6 +44,22 @@ public class ChannelManager implements Serializable {
 	@SuppressWarnings("rawtypes")
 	private Map stormConfig;
 
+	/**
+	 * Creates a new ChannelManager for a specific channel with a batcher
+	 * operation.
+	 * 
+	 * @param channelId
+	 *            The id of the channel this channelManager works for.
+	 * @param batcherClass
+	 *            The class of the batcher to be used.
+	 * @param batchOperationClass
+	 *            The class of the batched operation to be used.
+	 * @param conf
+	 *            A reference to the storm config
+	 * @param emitParticleHandler
+	 *            A reference to emit particles, to be used by
+	 *            metaParticlehandlers.
+	 */
 	public ChannelManager(String channelId,
 			Class<? extends Batcher> batcherClass,
 			Class<? extends BatchOperation> batchOperationClass,
@@ -46,6 +69,20 @@ public class ChannelManager implements Serializable {
 				emitParticleHandler);
 	}
 
+	/**
+	 * Creates a new ChannelManager for a specific channel with a single
+	 * operation.
+	 * 
+	 * @param channelId
+	 *            The id of the channel this channelManager works for.
+	 * @param batchOperationClass
+	 *            The class of the single operation to be used.
+	 * @param conf
+	 *            A reference to the storm config
+	 * @param emitParticleHandler
+	 *            A reference to emit particles, to be used by
+	 *            metaParticlehandlers.
+	 */
 	public ChannelManager(String channelId,
 			Class<? extends SingleOperation> operationClass,
 			@SuppressWarnings("rawtypes") Map conf,
@@ -54,6 +91,15 @@ public class ChannelManager implements Serializable {
 				emitParticleHandler);
 	}
 
+	/**
+	 * An internal class to create the channelManager
+	 * 
+	 * @param channelId
+	 * @param batcherClass
+	 * @param operationClass
+	 * @param conf
+	 * @param emitParticleHandler
+	 */
 	private void channelManager(String channelId,
 			Class<? extends Batcher> batcherClass,
 			Class<? extends Operation> operationClass,
@@ -67,18 +113,18 @@ public class ChannelManager implements Serializable {
 		this.emitParticleHandler = emitParticleHandler;
 
 		metaParticleHandlers = new ArrayList<MetaParticleHandler>();
-		System.out.println("Channel manager for channel " + channelId
-				+ " created");
+		logger.debug("Channel manager for channel " + channelId + " created");
 	}
 
 	/**
 	 * Process a particle (either MetaParticle or DataParticle)
 	 * 
 	 * @param particle
-	 *            Particle to be processed
-	 * @return returns a list with one MetaParticle, zero or more DataParticles
-	 *         or null in case of an error. These particles should be emitted by
-	 *         the bolt.
+	 *            Particle to be processed. If the particle == null, null is
+	 *            returned
+	 * @return returns a list with one MetaParticle (to be sent further upto the
+	 *         topology), zero or more DataParticles or null in case of an
+	 *         error. These particles should be emitted by the bolt.
 	 */
 	public List<Particle> processParticle(Particle particle) {
 		if (particle == null)
@@ -86,7 +132,8 @@ public class ChannelManager implements Serializable {
 
 		// make sure this channel manager has an operation and optional
 		// metaParticleHandlers
-		if (operation == null) {
+		if (operation == null) { // this is the first particle, no operation has
+									// been instantiated.
 			try {
 				createOperation(particle);
 			} catch (InstantiationException | IllegalAccessException e) {
@@ -99,21 +146,27 @@ public class ChannelManager implements Serializable {
 
 		// parse particles
 		List<Particle> result = new ArrayList<Particle>();
-		if (particle instanceof MetaParticle) {
+
+		if (particle instanceof MetaParticle) { // metaParticle
 			handleMetaParticle((MetaParticle) particle);
+			// add metaParticle to output list in order to be resent further in
+			// the topology
 			result.add(particle);
-		} else if (particle instanceof DataParticle) {
+
+		} else if (particle instanceof DataParticle) { // dataParticle
 			List<DataParticle> outputDataParticles = null;
-			if (batcher != null) {
+			if (batcher != null) { // batch dataParticle and give it to
+									// batcherOperation
 				List<DataParticleBatch> batchedParticles = batcher
 						.batch((DataParticle) particle);
+				// are there one or more batches to be sent?
 				if (batchedParticles != null) {
 					for (DataParticleBatch batchedParticle : batchedParticles) {
 						outputDataParticles = ((BatchOperation) operation)
 								.execute(batchedParticle);
 					}
 				}
-			} else {
+			} else { // single operation
 				outputDataParticles = ((SingleOperation) operation)
 						.execute((DataParticle) particle);
 			}
@@ -131,26 +184,50 @@ public class ChannelManager implements Serializable {
 		return result;
 	}
 
+	/**
+	 * Create a new operation with Batcher and metaParticleHandlers
+	 * 
+	 * @param firstParticle
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
 	private void createOperation(Particle firstParticle)
 			throws InstantiationException, IllegalAccessException {
-		batcher = batcherClass.newInstance();
-		batcher.init(firstParticle.getChannelId(),
-				firstParticle.getTimestamp(), stormConfig);
+		// create optional batcher
+		if (batcherClass != null) {
+			batcher = batcherClass.newInstance();
+			batcher.init(firstParticle.getChannelId(),
+					firstParticle.getTimestamp(), stormConfig);
+		}
 
+		// create new operation and initialize it
 		operation = operationClass.newInstance();
 		if (operationClass.isAssignableFrom(BatchOperation.class)) {
 			((BatchOperation) operation).init(firstParticle.getChannelId(),
 					firstParticle.getTimestamp(), stormConfig);
-		} else if (operationClass.isAssignableFrom(Operation.class)) {
+		} else if (operationClass.isAssignableFrom(SingleOperation.class)) {
 			operation.init(firstParticle.getChannelId(),
 					firstParticle.getTimestamp(), stormConfig);
+		} else {
+			logger.error("OperationClass of unknown type "
+					+ operationClass.getName() + ", expected: "
+					+ SingleOperation.class.getName() + " or "
+					+ BatchOperation.class.getName());
 		}
 
 		createMetaParticleHandlers(operation);
 	}
 
+	/**
+	 * Create all metaHandlers as specified in the annotations
+	 * 
+	 * @param operation
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
 	private void createMetaParticleHandlers(Operation operation)
 			throws InstantiationException, IllegalAccessException {
+		// walk through all specified metaHandlers in the annotation
 		OperationDeclaration operationDeclaration = operation.getClass()
 				.getAnnotation(OperationDeclaration.class);
 		for (Class<? extends MetaParticleHandler> mph : operationDeclaration
@@ -161,9 +238,18 @@ public class ChannelManager implements Serializable {
 		}
 	}
 
+	/**
+	 * pass this metaParticle to all metaHandlers.
+	 * 
+	 * @param metaParticle
+	 */
 	private void handleMetaParticle(MetaParticle metaParticle) {
 		for (MetaParticleHandler mph : metaParticleHandlers) {
-			mph.handleMetaParticle(metaParticle);
+			MetaParticleHandlerDecleration mphd = mph.getClass().getAnnotation(
+					MetaParticleHandlerDecleration.class);
+			if (metaParticle.getClass().isAssignableFrom(mphd.metaParticle())) {
+				mph.handleMetaParticle(metaParticle);
+			}
 		}
 	}
 
